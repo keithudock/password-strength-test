782264fa47cb3f14ecb11e8ce8106aa6b9eda825
Keith <keithudock@gmail.com>
2019-05-16 21:33:05 -0400

    Added a frame

--- passwordStrengthJava/GUIProgram.java
+++ passwordStrengthJava/GUIProgram.java
@@ -8,21 +8,24 @@ public class GUIProgram extends Frame implements ActionListener{
 
     public GUIProgram(){
 
+        //Create Frame
+        Frame f = new Frame("Password Strength Tester");
+
         // Create a check button
         Button btn = new Button("Check");
-        btn.setBounds(120,125,50,25);
+        btn.setBounds(150,125,50,25);
         btn.addActionListener(this);
-        add(btn);
+        f.add(btn);
 
         // Create a text field
         text = new TextField();
-        text.setBounds(95, 80, 100,25);
-        add(text);
+        text.setBounds(125, 80, 100,25);
+        f.add(text);
 
         // Set window size and visibility
-        setSize(300,200);//frame size 300 width and 300 height
-        setLayout(null);//no layout manager
-        setVisible(true);//now frame will be visible, by default not visible
+        f.setSize(350,200);//frame size 300 width and 300 height
+        f.setLayout(null);//no layout manager
+        f.setVisible(true);//now frame will be visible, by default not visible
     }
 
     public void actionPerformed(ActionEvent e){
