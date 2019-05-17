e8eea8fc17a5134f7c85f9bae167ac29d69e4afc
Keith <keithudock@gmail.com>
2019-05-16 21:28:01 -0400

    Moved passwordCheck to GUIProgram | Added button functionality

--- passwordStrengthJava/GUIProgram.java
+++ passwordStrengthJava/GUIProgram.java
@@ -1,16 +1,21 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class GUIProgram extends Frame {
+public class GUIProgram extends Frame implements ActionListener{
+    TextField text;
 
     public GUIProgram(){
 
         // Create a check button
         Button btn = new Button("Check");
         btn.setBounds(120,125,50,25);
+        btn.addActionListener(this);
         add(btn);
 
         // Create a text field
-        TextField text = new TextField();
+        text = new TextField();
         text.setBounds(95, 80, 100,25);
         add(text);
 
@@ -18,5 +23,41 @@ public class GUIProgram extends Frame {
         setSize(300,200);//frame size 300 width and 300 height
         setLayout(null);//no layout manager
         setVisible(true);//now frame will be visible, by default not visible
+    }
+
+    public void actionPerformed(ActionEvent e){
+        passwordCheck(text.getText());
+    }
+
+    public static void passwordCheck(String password) {
+        Pattern lower = Pattern.compile(".*[a-z].*");
+        Pattern upper = Pattern.compile(".*[A-Z].*");
+        Pattern number = Pattern.compile(".*[1-9].*");
+
+        Matcher lowerMatch = lower.matcher(password);
+        Matcher upperMatch = upper.matcher(password);
+        Matcher numberMatch = number.matcher(password);
+
+        if (password.length() >= 8) {
+            if (lowerMatch.find()) {
+                if (upperMatch.find()) {
+                    if (numberMatch.find()){
+                        System.out.println("Good!");
+                    }
+                    else {
+                        System.out.println("Password is not strong enough | Need at least one number");
+                    }
+                }
+                else {
+                    System.out.println("Password is not strong enough | Need at least one uppercase letter");
+                }
+            }
+            else {
+                System.out.println("Password is not strong enough | Need at least one lowercase letter");
+            }
+        }
+        else {
+            System.out.println("Password is not strong enough | Need at least eight characters");
+        }
     }
 }
